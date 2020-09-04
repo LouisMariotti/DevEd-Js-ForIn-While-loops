e63fe3407c7a3eb81baadc69b61299f329bddcfd
LouisMariotti <68160210+LouisMariotti@users.noreply.github.com>
2020-09-04 14:46:24 +0200

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,34 @@
+// // To loop with objects we are goin to use for in
+
+// // const user = {
+// //   name: "Louis",
+// //   age: 19,
+// //   subscribers: 200,
+// //   money: 30,
+// // };
+
+// // for (let x in user) {
+// //   console.log(user[x]);
+// // }
+
+// // this is a for in loop especcially used for objects
+
+// console.log(user.subscribers); // Will display the subscribers value
+
+// // also have While and Do while loops, with these ones we have to declare a variable outside
+
+// let i = 20;
+
+// while (i <= 300) {
+//   console.log(i);
+//   i += 10; // we increment it 10 by 10
+// } // make sure to increment everything
+
+// Do while is different cuz its going to run even if our statement is false
+
+let i = 0;
+
+do {
+  console.log(i);
+  i++;
+} while (i > 10); // this is false but it's still going to run once
